4250c54b773c96984f3e5bf4783f2fcdf28bed0d
Davit Svanidze <davit.svanidze@stat-up.com>
2024-11-01 19:36:00 +0100

    Initial example

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import ReactDOM from 'react-dom'
+import { Logo } from '@pmndrs/branding'
+import './styles.css'
+import App from './App'
+
+function Overlay() {
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
+      <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
+        pmnd.rs
+        <br />
+        dev collective
+      </a>
+      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate3d(-50%,-50%,0)' }}>
+        <h1 style={{ margin: 0, padding: 0, fontSize: '15em', fontWeight: 500, letterSpacing: '-0.05em' }}>Hello People!</h1>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <>
+    <App /> <Overlay />
+    <Logo style={{ position: 'absolute', bottom: 40, left: 40, width: 30 }} />
+  </>,
+  document.getElementById('root')
+)
